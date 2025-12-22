74ed0487a0dfce560384eb319fb9e7fa609b7a41
wykiesautomation <wykiesautomation@gmail.com>
2025-12-22 20:17:43 +0200

    Delete assets directory

--- assets/js/env.js
+++
@@ -1,6 +0,0 @@
-
-window.__env = {
-  SHEET_API_URL: 'https://script.google.com/macros/s/AKfycbww5CBY6Gbwmbkqvydn7FoC8ZZD1JjU70PuBAvG4BdBlxcT6EakV8KQKfWzzR7I-lJBEw/exec',
-  ADMIN_PASSPHRASE: 'Ford@20132016',
-  WHATSAPP_NUMBER_INTL: '27716816131'
-};
