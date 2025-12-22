f81a763371ff6c10c1115f87f3eec1f096b8f6a3
wykiesautomation <wykiesautomation@gmail.com>
2025-12-22 18:45:47 +0200

    Update env.js

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,6 +1,6 @@
 
 window.__env = {
-  SHEET_API_URL: '',
-  ADMIN_PASSPHRASE: '',
+  SHEET_API_URL: 'https://script.google.com/macros/s/AKfycbww5CBY6Gbwmbkqvydn7FoC8ZZD1JjU70PuBAvG4BdBlxcT6EakV8KQKfWzzR7I-lJBEw/exec',
+  ADMIN_PASSPHRASE: 'Ford@20132016',
   WHATSAPP_NUMBER_INTL: '27716816131'
 };
