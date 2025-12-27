ca02c9d1e858d484c05676ef8cf0ccc47486dc51
wykiesautomation <wykiesautomation@gmail.com>
2025-12-27 22:10:22 +0200

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,5 +0,0 @@
-
-function openWhatsApp(sku){
-  const msg = encodeURIComponent('Hi Wykies Automation, I am interested in ' + sku + ' from your website.');
-  window.open('https://wa.me/27716816131?text=' + msg, '_blank');
-}
